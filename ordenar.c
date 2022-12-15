e0ea81e3afef3b772596aa7d883bafdc30ec3731
Juan Marcelo Ascuna Isla <juan.ascunaisla@gmail.com>
2022-12-14 21:04:24 -0500

    Segundo commit

--- ordenar.c
+++ ordenar.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void burbuja (int arreglo[], int longitud);
+void intercambiar(int* a, int* b);
+
 int main()
 {
     int num_ordenar[] = {30, 25, 12, 34,-4, -53, 56, 200, 43, 28};
@@ -17,4 +20,36 @@ int main()
     }
 
     printf("\n");
+
+    burbuja(num_ordenar,longitud_arreglo);
+
+    printf("Datos del arreglo ordenados: ");
+
+    for(int i = 0; i < longitud_arreglo ; i++ )
+    {
+        printf("%d ",num_ordenar[i]);
+    }
+
+    printf("\n");
+}
+
+void burbuja(int arreglo[], int longitud)
+{
+    for(int j = 0; j < longitud; j++)
+    {
+        for(int k = 0; k < longitud-j-1; k++)
+        {
+            if (arreglo[k] > arreglo[k+1])
+            {
+                intercambiar(&arreglo[k],&arreglo[k+1]);
+            }
+        }
+    }
+}
+
+void intercambiar(int* a,int* b)
+{
+    int temporal = *a;
+    *a = *b;
+    *b = temporal;
 }
